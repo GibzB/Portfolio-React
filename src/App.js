ff03c7398360c1a4ef606037d6e35e59c1647e0e
Billy Gibendi <RGibesh@users.noreply.github.com>
2022-02-28 22:00:11 +0300

    Added a paragraph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
 <h1>This is my first react app </h1>
+    <p> So this will be my first react project and learning while on the go! I will be using VScode to do my code editing and push changes via git<p>
     </div>
   );
 }
