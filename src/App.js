674c09a674a7469e2c7a0ed95faa9abde32c5e95
RGibesh <ralphgibendi01@gmail.com>
2022-03-01 21:34:09 +0300

    added button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
-import './App.css';
 import React, { useState } from 'react'
 import data from './data'
 import List from './List'
 function App() {
-	// Edit your return statement here to match the instructions
+	// create a state variable here
+	const [people, setPeople] = useState(data)
+
+	// this should clear all records
+	function clearAllRecords() {
+		setPeople([])
+	}
 	return (
 		<main>
 			<section className="container">
-				<h3>0 birthdays today</h3>
-				<List />
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={clearAllRecords}>Clear All</button>
 			</section>
 		</main>
 	)
