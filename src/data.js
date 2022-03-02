5eed518180567fc55c090f9adf48f4b992341ebf
Billy Gibendi <RGibesh@users.noreply.github.com>
2022-03-02 23:43:16 +0300

    age change

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export default [
 	{
 		id: 1,
 		name: 'Raila Odinga',
-		age: 28,
+		age: 78,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
 	},
 	{
 		id: 2,
 		name: 'Reuben Kigame',
-		age: 32,
+		age: 62,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg'
 	},
 	{
 		id: 3,
 		name: 'William Samoei Rutto',
-		age: 36,
+		age: 57,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg'
 	},
 	{
 		id: 4,
 		name: 'Akuro Aukot',
-		age: 34,
+		age: 46,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg'
 	},
 	{
 		id: 5,
 		name: 'Mwangi wa Iria',
-		age: 29,
+		age: 47,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg'
 	}
 ]
