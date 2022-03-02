4e1194d53f8c2f8954124f2d84f9cf545d012a60
Billy Gibendi <RGibesh@users.noreply.github.com>
2022-03-02 23:42:16 +0300

    name changes

--- src/data.js
+++ src/data.js
@@ -1,31 +1,31 @@
 export default [
 	{
 		id: 1,
-		name: 'Bertie Yates',
+		name: 'Raila Odinga',
 		age: 28,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
 	},
 	{
 		id: 2,
-		name: 'Hester Hogan',
+		name: 'Reuben Kigame',
 		age: 32,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg'
 	},
 	{
 		id: 3,
-		name: 'Larry Little',
+		name: 'William Samoei Rutto',
 		age: 36,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg'
 	},
 	{
 		id: 4,
-		name: 'Sean Walsh',
+		name: 'Akuro Aukot',
 		age: 34,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg'
 	},
 	{
 		id: 5,
-		name: 'Lola Gardner',
+		name: 'Mwangi wa Iria',
 		age: 29,
 		image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg'
 	}
