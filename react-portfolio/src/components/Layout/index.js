5acef53d4da306a4b32a6663a89d4142af069abb
Billy Gibendi <ralphgibendi01@gmail.com>
2023-03-12 20:43:56 +0300

    images added to assets

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -2,7 +2,10 @@ import './index.scss';
 
 const Layout = () => {
     return (
-         <h3>React Portfolio</h3>
+         <><h3>React Portfolio</h3>
+        
+            <div className="background-image"></div></>
+
     )
     }
 
